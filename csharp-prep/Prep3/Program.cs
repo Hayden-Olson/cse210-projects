faa088cda551a29a2cbfbb0634895680a7ff81a5
Hayden Olson <haydenolson@byui.edu>
2025-01-16 10:52:16 -0700

    wrote the code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "";
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+            int guess = -1;
+            int attempt = 0;
+            Console.WriteLine("What is the magic number?");
+            while (guess != number);
+                attempt += 1;
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            if (attempt > 1)
+            {
+                Console.WriteLine($"It took you {attempt} tries!");
+            }
+            else if (attempt == 1)
+            {
+                Console.WriteLine("First try!");
+            }
+            Console.Write("Do you wanna play again? (yes/no) ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
