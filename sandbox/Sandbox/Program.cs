f18c2d745917166d49278e052776d4baf5456da4
Hayden Olson <haydenolson@byui.edu>
2025-01-21 16:30:24 -0700

    worked with classes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,39 +1,32 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Not adj. We want the variable to be clear.
-        string adjective = GetAdjective();
-        string noun = GetNoun();
+        Movie favoriteMovie = new Movie();
+        favoriteMovie._title = "Star Wars";
+        favoriteMovie._year = 1977;
+        favoriteMovie._runtime = 150;
+        favoriteMovie._rating = "PG";
 
-        int number = Multiply(3,4);
+        Movie otherMovie = new Movie();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._rating = "PG-13";
+        otherMovie._runtime = 162;
 
-        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}s.");
-        List<string> words = new List<string>();
-        words.Add("blue");
-        words.Add("yellow");
-        words.Add("big");
-        words.Add("small");
-        
+        favoriteMovie.Display();
+
+        DisplayMovie(favoriteMovie);
+        DisplayMovie(otherMovie);
+
+        Console.WriteLine(favoriteMovie._title);
     }
-    static int Multiply(int number1, int number2)
-    {
-        int product = number1 * number2;
 
-        return product;
-    }
-    static string GetAdjective()
+    static void DisplayMovie(Movie aMovie)
     {
-        string adjective = "yellow";
-
-        return adjective;
-    }
-    static string GetNoun()
-    {
-        string word = "bird";
-
-        return word;
+        Console.WriteLine($"{aMovie._title} - {aMovie._year}");
     }
 }
