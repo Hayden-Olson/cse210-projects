53d694ef4b025ca86e1c17baad371880f2e734ef
Hayden Olson <haydenolson@byui.edu>
2025-03-18 17:49:41 -0600

    finished the program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Blue", 20));
+        shapes.Add(new Rectangle("Red", 12, 13));
+        shapes.Add(new Circle("Purple", 12));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.\n");
+        }
     }
 }
